33d16f73c9bd4a16ed11f5471b141cb44fc4bf0f
LightGuard <lightguard.jp@gmail.com>
2010-08-26 23:04:49 -0600

    Continuing with script port
    Need to finish fix_tags (now convert_tags)
    Need to add in Dan's other gist

--- seam_git_migration.groovy
+++ seam_git_migration.groovy
@@ -23,26 +23,61 @@ Thread.start {
     shell.svn('list', '--xml', 'http://anonsvn.jboss.org/repos/seam/modules/').toStream(out)
 }
 
-new XmlSlurper().parse(_in).list.entry.name.collect { it.text() }.each {
+def modules = XmlSlurper().parse(_in).list.entry.name.collect { it.text() }
+['parent', 'dist'].each { modules << it}
+
+modules.each {
 
     shell.mkdir('-p', it)
     shell.cd(it)
 /*    
     if (it == 'wicket')
-        shell.git('svn', 'clone', "http://anonsvn.jboss.org/repos/seam/modules/${it}", '--no-metadata', '--no-minimize-url', '--trunk', '--tags', '--authors-file=../../svn.authors').waitForExit()
+        git_clone(false)
     else
-        shell.git('svn', 'clone', "http://anonsvn.jboss.org/repos/seam/modules/${it}", '--no-metadata', '--no-minimize-url', '--trunk', '--authors-file=../../svn.authors').waitForExit()
-*/    
-    fix_tags()    
+        git_clone()
+    convert_tags()    
+    git_clean()
+*/      
+    shell.cd("../${phase2_dir}")
 /*
+    git_clone_push(it) // TODO: add conditions
+*/
+}
+
+def git_clone(includeTags = true )
+{
+    def args = ['svn', 'clone', "http://anonsvn.jboss.org/repos/seam/modules/${it}", '--no-metadata', '--no-minimize-url', '--trunk', '--tags', '--authors-file=../../svn.authors']
+
+    if (!includeTags) 
+        args - '--tags'
+    
+    shell.git(args).waitForExit()
+}
+
+def git_clean()
+{
     shell.git('reflog', 'expire', '--all', '--expire=now')
     shell.git('gc', '--aggressive')
     shell.git('prune')
     shell.git('fsck', '--full')
-*/      
-    shell.cd('..')
 }
 
-def fix_tags() {
-
+def convert_tags() {
+    shell.git('for-each-ref', "--format='%(refname)'", "refs/remotes/tags").eachLine {
+        tag = it - 'refs/remots/'
+        tree = shell.git('rev-parse', it).text
+        
+        // TODO: finish fix_tags from old script
+    }
 } 
+
+def git_clone_push(module, newName = moduleName)
+{
+    shell.git('clone', "../${phase1_dir}/${module}", newName)
+    shell.cd(module)
+    shell.git('remote', 'add', 'github', "git@github.com:seam/${newName}.git")
+    shell.git('remote', '-v')
+    shell.git('push', '--mirror', 'github', 'master')
+}
+
+// vim:ts=4:sw=4:sts=4
